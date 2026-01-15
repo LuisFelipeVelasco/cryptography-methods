0227f388a05037d433842b8de466173d16ce9b02
LuisFelipeVelasco <luisfelipevelascochilito@gmail.com>
2026-01-15 10:29:55 -0500

    create an endpoit to use Atbash_cipher module

--- atbash_cipher.py
+++ atbash_cipher.py
@@ -5,18 +5,15 @@ corresponding letter from the reversed alphabet.
 The same algorithm is used for both encryption and decryption.
 
 """
-alphabet="abcdefghijklmnopqrstuvwxyz "
-cipher_alphabet="ZYXWVUTSRQPONMLKJIHGFEDCBA "
 
-message=input("Typer the text to crypt/decrypt: ")
-output=""
-
-# Iterate over each character in the message and substitute it
-# with the character at the same index in the cipher alphabet
-
-for character in message.lower():
-    if character in alphabet:
-        cipher=cipher_alphabet[alphabet.index(character)]
-        output+=cipher
-
-print(output)
+def codify(message):
+    alphabet="abcdefghijklmnopqrstuvwxyz "
+    cipher_alphabet="ZYXWVUTSRQPONMLKJIHGFEDCBA "
+    output=""
+    # Iterate over each character in the message and substitute it
+    # with the character at the same index in the cipher alphabet
+    for character in message.lower():
+        if character in alphabet:
+            cipher=cipher_alphabet[alphabet.index(character)]
+            output+=cipher
+    return output
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask , jsonify
+import atbash_cipher as atbash
 app= Flask(__name__)
-
-
 
 @app.route("/")
 def home():
     return "Hola"
+
+@app.route("/atbash/<message>")
+def atbash_cipher(message):
+    message=atbash.codify(message)
+    return jsonify(message),200
 
 if __name__=="__main__":
     app.run(debug=True)
