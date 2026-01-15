896d73007633e57d6ddf8930d0a8b9de1b298624
LuisFelipeVelasco <luisfelipevelascochilito@gmail.com>
2026-01-15 11:32:23 -0500

    Add endpoint to encrypt or decrypt messages using Caesar cipher

--- Cesar/cesar_cipher.py
+++ Cesar/cesar_cipher.py
@@ -12,28 +12,30 @@ to 'C'.
 
 """
 
-alphabet="abcdefghijklmnopqrstuvwxyz"
-
-user_action=input("Do you want decrypt (D) or encrypt (C)?:  ")
-key=int(input("Type the key: "))
-
-message=input("Type the message to encrypt/decrypt ")
-output=""
-
 # Iterate over each character in the message to find its position in the alphabet
 # If the user chooses decryption, subtract the key from the character position
 # If the user chooses encryption, add the key to the character position
 # Use modulo arithmetic to wrap around the alphabet length
 
-for character in message.lower():
-        if character in alphabet:
-            character_position=alphabet.find(character)
-            if user_action=="C":
-                  character_position= (character_position + key) % len(alphabet)
-            if user_action=="D":
-                  character_position= (character_position - key) % len(alphabet)
-            output+=alphabet[character_position]
-        else :
-            output+=character
+alphabet="abcdefghijklmnopqrstuvwxyz"
+def encrypt(key,message):  
+      output=""
+      for character in message.lower():
+            if character in alphabet:
+                  character_position=alphabet.find(character)
+                  character_position= (character_position + int(key) % len(alphabet))
+                  output+=alphabet[character_position]
+            else :
+                  output+=character
+      return output
 
-print(output)
+def decrypt(key,message):
+      output=""     
+      for character in message.lower():
+            if character in alphabet:
+                  character_position=alphabet.find(character)
+                  character_position= (character_position - int(key) % len(alphabet))
+                  output+=alphabet[character_position]
+            else :
+                  output+=character
+      return output
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask , jsonify
 import atbash_cipher as atbash
+import Cesar.cesar_cipher as Cesar
 from flask_cors import CORS
 
 app= Flask(__name__)
@@ -9,10 +10,20 @@ CORS(app)
 def home():
     return "Hola"
 
-@app.route("/atbash/<message>")
-def atbash_cipher(message):
-    message=atbash.codify(message)
+@app.route("/atbash/<user_message>")
+def atbash_cipher(user_message):
+    message=atbash.codify(user_message)
     return jsonify({"result":message}),200
+
+@app.route("/cesar_encrypt/<key>/<user_message>")
+def cesar_encrypt(key,user_message):
+    message=Cesar.encrypt(key,user_message)
+    return jsonify({"result":message}),200
+
+@app.route("/cesar_decrypt/<key>/<user_message>")
+def cesar_decrypt(key,user_message):
+    message=Cesar.decrypt(key,user_message)
+    return jsonify({"result":message})
 
 if __name__=="__main__":
     app.run(debug=True)
