cdc29bc7bf709885a5f55a6cc0552eaad64ad039
LuisFelipeVelasco <luisfelipevelascochilito@gmail.com>
2026-01-15 10:36:26 -0500

    Use CORS to avoid port issues

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask , jsonify
 import atbash_cipher as atbash
+from flask_cors import CORS
+
 app= Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def home():
@@ -9,7 +12,7 @@ def home():
 @app.route("/atbash/<message>")
 def atbash_cipher(message):
     message=atbash.codify(message)
-    return jsonify(message),200
+    return jsonify({"result":message}),200
 
 if __name__=="__main__":
     app.run(debug=True)
