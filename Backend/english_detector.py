1c69158e89cc8da013e93071406cc2200ab17853
LuisFelipeVelasco <luisfelipevelascochilito@gmail.com>
2026-02-22 20:04:16 -0500

    fix error of english_dictionary.txt address

--- Backend/english_detector.py
+++ Backend/english_detector.py
@@ -2,12 +2,14 @@
 #Read the english_dictionary.txt and make a list of all the words in the file
 #Take each possible word of the user_text and check if is in the list of english_words
 #Return a string with the words in the user_text that are in english
+import os 
 
 def Detect_Number_Of_Words_In_English(user_text):
-    file_path = "english_dictionary.txt"
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(base_dir, "english_dictionary.txt")   
     with open(file_path, "r", encoding="utf-8") as f:
-        text = f.read()
-    words = text.split()
+        dictionary = f.read()
+    dictionary=dictionary.split()
     lenght_of_user_text=len(user_text)
     list_of_user_words_in_English=[]
     starting_position_of_word_to_check = 0
@@ -17,8 +19,8 @@ def Detect_Number_Of_Words_In_English(user_text):
         quantity_of_characters_of_word_to_check = 4
         while quantity_of_characters_of_word_to_check <= maximum_number_of_characters_in_a_word and (quantity_of_characters_of_word_to_check + starting_position_of_word_to_check) <= lenght_of_user_text:
             word = user_text[starting_position_of_word_to_check:starting_position_of_word_to_check + quantity_of_characters_of_word_to_check]
-
-            if word in words:
+            print(word)
+            if word in dictionary:
                 list_of_user_words_in_English.append(word)
                 starting_position_of_word_to_check += quantity_of_characters_of_word_to_check
                 break
